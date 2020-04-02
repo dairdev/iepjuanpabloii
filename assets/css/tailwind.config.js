90bfa07a7272a3dfe1358f77fcbacf04a18041de
Dennis <dennis.dair@gmail.com>
2020-04-02 09:38:29 -0500

    tailwind carrousel implementation

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    theme: {
+	fontFamily: {
+	    sans: ['Raleway', 'sans-serif']
+	},
+	extend: {}
+    },
+    variants: {},
+    plugins: []
 }
